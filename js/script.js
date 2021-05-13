b5a1e2c316a33c0f90a6f256a319c3f2dc5893de
alexandra2a <alexandra.conforto@gmail.com>
2021-05-13 15:55:59 +0200

    :sparkles: Draw multiple rectangles and check browsers [2hours]

--- js/script.js
+++ js/script.js
@@ -1,52 +1,97 @@
 const canvas = document.getElementById('canvas');
-var mouse = {
+let elementsList = [];
+let curentElement = null;
+let mouse = {
     x: 0,
-    y: 0,
-    startX: 0,
-    startY: 0
+    y: 0
 };
+
+//click
+canvas.onmousedown = function (e) {
+    curentElement = createDiv("Div" + elementsList.length, mouse.x, mouse.y)
+    canvas.appendChild(curentElement)
+}
+
+//drag
+canvas.onmousemove = function (e) {
+    setMousePosition(e);
+    if (curentElement !== null) {
+        curentElement.style.width = Math.abs(mouse.x - curentElement.startX) + 'px';
+        curentElement.style.height = Math.abs(mouse.y - curentElement.startY) + 'px';
+        curentElement.style.left = (mouse.x - curentElement.startX < 0) ? mouse.x + 'px' : curentElement.startX + 'px';
+        curentElement.style.top = (mouse.y - curentElement.startY < 0) ? mouse.y + 'px' : curentElement.startY + 'px';
+    }
+}
+
+//drop
+canvas.onmouseup = function (e) {
+    elementsList.push(curentElement);
+    curentElement = null;
+}
 
 function setMousePosition(ev) {
     mouse.x = ev.pageX + window.pageXOffset;
     mouse.y = ev.pageY + window.pageYOffset;
-    document.getElementById('mouse').innerText = mouse.x + ' ' + mouse.y;
 };
 
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-var element = null;
+function isAllRotationDone() {
+    const rectDivList = document.querySelectorAll(".rectangle")
+    for (let i = 0; i < rectDivList.length - 1; i++) {
+        if (rectDivList[i].rotationInProgress) {
+            return false;
+        }
+    }
 
-canvas.onmousemove = function (e) {
-    setMousePosition(e);
-    if (element !== null) {
-        element.style.width = Math.abs(mouse.x - mouse.startX) + 'px';
-        element.style.height = Math.abs(mouse.y - mouse.startY) + 'px';
-        element.style.left = (mouse.x - mouse.startX < 0) ? mouse.x + 'px' : mouse.startX + 'px';
-        element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
+    return true;
+}
+
+function removeAllRotationDone() {
+    var el = document.querySelectorAll(".rectangle")
+    for (var i = 0, ilen = el.length - 1; i < ilen; i++) {
+        if (el[i].rotationDone) {
+            canvas.removeChild(el[i])
+        }
     }
 }
 
-canvas.onclick = function (e) {
-    if (element !== null) {
-        element = null;
-        canvas.style.cursor = "default";
-        console.log("finsihed.");
-    } else {
-        console.log("begun.");
-        mouse.startX = mouse.x;
-        mouse.startY = mouse.y;
-        element = document.createElement('div');
-        element.setAttribute("id", "div0");
-        element.className = 'rectangle'
-        element.style.left = mouse.x + 'px';
-        element.style.top = mouse.y + 'px';
-        canvas.appendChild(element)
-        canvas.style.cursor = "crosshair";
-    }
-}
+// create rectangle div with dblclick event
+function createDiv(id, left, top, width = 0, height = 0) {
+    const rotationDurationInSeconds = 3;
+    let element = {};
+    element = document.createElement('div');
+    element.setAttribute("id", id);
+    element.className = 'rectangle';
+    element.style.background = "rgb(" + randomIntFromInterval(0, 255) + ", " + randomIntFromInterval(0, 255) + ", " + randomIntFromInterval(0, 255) + ")";
+    element.style.left = left + 'px';
+    element.style.top = top + 'px';
+    element.rotationInProgress = false;
+    element.rotationDone = false;
+    element.startX = left;
+    element.startY = top;
 
-document.getElementById('rotate').onclick = function (e) {
-    setInterval(function () {
-        document.getElementById('div0').style.WebkitTransitionDuration = '1s';
-        document.getElementById('div0').style.webkitTransform = 'rotate(360deg)'; // not obsolete in the W3C Recommendation.
-    }, 100);
-}
+    // dblclick event must rotate and remove the div
+    element.ondblclick = function (e) {
+        // because the click event had create a div, we delete it
+        elementsList.pop();
+
+        // rotation
+        this.style.WebkitTransitionDuration = rotationDurationInSeconds + 's';
+        this.style.webkitTransform = 'rotate(360deg)'; // not obsolete in the W3C Recommendation.
+        this.rotationInProgress = true;
+
+        // remove, at the end of rotation
+        setTimeout(() => {
+            this.rotationInProgress = false;
+            this.rotationDone = true;
+            if (isAllRotationDone()) {
+                removeAllRotationDone();
+            }
+        }, rotationDurationInSeconds * 1000);
+    }
+
+    return element;
+};
